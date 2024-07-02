04339ffbd6c64e108b01b81a37eec8aff1302719
AtheekJAR <contactatheekjar@gmail.com>
2024-07-02 13:29:44 +0530

    just cloned the repo insted of forking and clone so all the files are modified at the same time and getting push to this repo!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fValue) {
+  return Math.round(5/9 * (fValue - 32)* 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cValue) {
+  return Math.round(((cValue * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
