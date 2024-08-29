5d4c10aa37a2852908284054a01f01565533b091
AtheekJAR <contactatheekjar@gmail.com>
2024-08-29 20:06:07 +0530

    sum function is now working!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(n1, n2) {
 	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(value, number){
+    return value + number;
+  },0)
 };
 
 const multiply = function() {
