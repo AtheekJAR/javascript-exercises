b9ce50831e9f19e2bcdbdc2f0c66db9eba5de63f
AtheekJAR <contactatheekjar@gmail.com>
2024-09-02 03:08:58 +0530

    wrote a greate solution for palindromes challange..!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    return  input.toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"") === input.split("").reverse().join("").toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
 };
 
 // Do not edit below this line
