66e690a9fdef9531cee928610d18fa9b900cb796
AtheekJAR <contactatheekjar@gmail.com>
2024-09-07 21:57:11 +0530

    it's done using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0) {
+        return "OOPS"
+    } else if(index <= 1){
+        return Number(index)
+    }else{
+        return Number(fibonacci(index-1) + fibonacci(index - 2));
+    }
 };
 
 // Do not edit below this line
