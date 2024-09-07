3864d0dcf8e1e9c60ef7033a3c3606d386737d63
AtheekJAR <contactatheekjar@gmail.com>
2024-09-08 02:44:15 +0530

    it's done using sort() and default operator!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    sortedArr = arr.sort(function(personA, personB){
+        const A_age = (personA.yearOfDeath || new Date().getFullYear()) - personA.yearOfBirth;
+		const B_age = (personB.yearOfDeath || new Date().getFullYear()) - personB.yearOfBirth;
+        return B_age - A_age;
 
+    })
+    return sortedArr[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
